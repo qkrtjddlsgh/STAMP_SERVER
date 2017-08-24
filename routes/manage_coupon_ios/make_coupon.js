cd0ea695466a2f31fe5cf637a38151c4dbf05e2f
LAPTOP-Q9MM1J13\qkrtj <glgl2131@inha.edu>
2017-08-24 14:57:20 +0900

    coupon 모듈 수정

--- routes/manage_coupon_ios/make_coupon.js
+++ routes/manage_coupon_ios/make_coupon.js
@@ -5,28 +5,21 @@ var coupon = require('../../models/Coupon');
 router.post('/', function(req, res){
     var recv_data = req.body;
 
-    var store_type = recv_data.store_type;
-    var store_region = recv_data.store_region;
     var member_id = recv_data.member_id;
     var member_phone_number = recv_data.member_phone_number;
-    //var num_of_stamp = recv_data.num_of_stamp;
 
-    coupon.find({member_id: member_id, store_type: store_type, store_region: store_region}, function(err, doc){
+    coupon.find({member_id: member_id, member_phone_number: member_phone_number}, function(err, doc){
         if(err){
             console.error(err.message);
         }
         if(doc.length == 0){
             var new_coupon = new coupon();
-            new_coupon.store_type = store_type;
-            new_coupon.store_region = store_region;
+            new_coupon.doc_type = null;
             new_coupon.member_id = member_id;
             new_coupon.member_phone_number = member_phone_number;
-            new_coupon.stamp_field = [];
-
-            /*for(var i=0; i<num_of_stamp; i++){
-                new_coupon.stamp_field[i] = 0;
-            }*/
-
+            new_coupon.inha_street = [];
+            new_coupon.bupyeong_street = [];
+            new_coupon.chinatown_street = [];
             new_coupon.save();
 
             var res_data = new Object();
@@ -36,7 +29,7 @@ router.post('/', function(req, res){
             res.send(res_data);
             res.end();
         }
-        else {
+        else{
             var res_data = new Object();
             res_data.code = "5200";
             res_data.message = "Coupon is already exist";
--- routes/manage_coupon_ios/show_coupon.js
+++ routes/manage_coupon_ios/show_coupon.js
@@ -5,11 +5,10 @@ var coupon = require('../../models/Coupon');
 router.post('/', function(req, res){
     var recv_data = req.body;
 
-    var store_type = recv_data.store_type;
-    var store_region = recv_data.store_region;
     var member_id = recv_data.member_id;
+    var member_phone_number = recv_data.member_phone_number;
 
-    coupon.find({member_id: member_id, store_type: store_type, store_region: store_region}, function(err, doc){
+    coupon.find({member_id: member_id, member_phone_number: member_phone_number}, function(err, doc){
        if(err){
            console.error(err.message);
        }
