1d84fb6ab73f830574a3be925a1f4ac3bc357d04
Park-Ryeong-Min <somuch0321@gmail.com>
2017-08-24 18:58:52 +0900

    임시 커밋 4

--- routes/req_list_or_data/getMerchantDetailData.js
+++ routes/req_list_or_data/getMerchantDetailData.js
@@ -5,10 +5,9 @@ var dberr = require('../../error_handle_modules/handleDBerror');
 
 router.post('/', function (req, res) {
     var recv_data = req.body;
-    var id = recv_data.id;
     var store_name = recv_data.store_name;
 
-    var find_query = {id : id, store_name : store_name};
+    var find_query = {store_name : store_name};
 
     merchant.find(find_query, function (err, result) {
         if(err){
