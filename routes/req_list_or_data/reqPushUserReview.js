6a707cfecdea17c338334a12e63f2edeca19ced3
LAPTOP-Q9MM1J13\qkrtj <glgl2131@inha.edu>
2017-08-25 12:00:03 +0900

    ㅂㅂㅂ

--- routes/req_list_or_data/reqPushUserReview.js
+++ routes/req_list_or_data/reqPushUserReview.js
@@ -35,14 +35,14 @@ router.post('/', function (req, res) {
         }else{
             res.send({result : result, review_data : review_obj});
             res.end();
-            merchant.find(find_query, function(err, result){
-                console.log(JSON.stringify(result));
-               var pre_rating = result[0].cur_rating;
-               pre_rating *= result[0].rate_user_number;
+            merchant.find(find_query, function(err, doc){
+                console.log(JSON.stringify(doc));
+               var pre_rating = doc[0].cur_rating;
+               pre_rating *= doc[0].rate_user_number;
                var cur_rating = pre_rating + parseInt(rating);
-               cur_rating /= result[0].rate_user_number + 1;
-               var rate_update_query = {$set : {rate_user_number : result[0].rate_user_number + 1, cur_rating:cur_rating}};
-               merchant.update(find_query, rate_update_query,function (err, result) {
+               cur_rating /= doc[0].rate_user_number + 1;
+               var rate_update_query = {$set : {rate_user_number : doc[0].rate_user_number + 1, cur_rating:cur_rating}};
+               merchant.update(find_query, rate_update_query,function (err, doc) {
 
                });
             });
